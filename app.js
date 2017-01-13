0f89e86e45ef186e33d110e76a844fc54a1dc384
suelton <suelton@gmail.com>
2017-01-13 10:44:23 -0300

    Atualização e inclusão de módulos

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = require("./config/server");
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require("./app/routes/home")(app);
+var rotaFormularioInclusaoNoticia = require("./app/routes/formulario_inclusao_noticia")(app);
+
+app.listen(3000, function(){
+    console.log("Servidor Rodando com Express");    
+});
